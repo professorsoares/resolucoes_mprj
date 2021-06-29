1d8753a14f753d1213ce621302c2cdd5cddf72bf
professorsoares <professorsoares@gmail.com>
2021-06-29 19:39:34 -0300

    Pandas Profiling

--- PandasProfiling_teste.py
+++ PandasProfiling_teste.py
@@ -4,23 +4,13 @@
 # # Simple Exploratory Data Analysis with Pandas-Profiling Package Python
 #     ## Exemplo demonstrado no vídeo:
 #         ## https://www.youtube.com/watch?v=773zrwAkmQ0
-
 # In[3]:
-
-
-get_ipython().system('pip install pandas-profiling')
-
-
-# In[8]:
-
+# get_ipython().system('pip install pandas-profiling')
 
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-
 # In[11]:
-
-
 #data = pd.read_excel('../flight_Data_Train.xlsx')
 #data = pd.read_csv('../wine_quality_class_cat.csv')
 
@@ -29,10 +19,7 @@ data = pd.read_csv('D:/ProjetosGithub/kaggle_flight_delays/flights.csv',
                  #lineterminator='\n')
 data.head()
 
-
 # In[14]:
-
-
 profile = ProfileReport(data, title='Perfil Modelo de Preços de Vôos', html={'style': {'full_width':True}})
 profile
 
